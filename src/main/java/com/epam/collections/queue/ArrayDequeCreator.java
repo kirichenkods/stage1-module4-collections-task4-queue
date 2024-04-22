cc01b5fd734d834d6a5f0f11358996e0bb95fa36
kirichenkods <kirichenkods@ya.ru>
2024-04-22 22:57:47 +0300

    add solution

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,23 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.add(firstQueue.remove());
+        deque.add(firstQueue.remove());
+        deque.add(secondQueue.remove());
+        deque.add(secondQueue.remove());
+        while (firstQueue.iterator().hasNext()
+                || secondQueue.iterator().hasNext()) {
+            moveCards(firstQueue, deque);
+            moveCards(secondQueue, deque);
+        }
+
+        return deque;
+    }
+
+    private void moveCards(Queue<Integer> queue, ArrayDeque<Integer> deque) {
+        queue.offer(deque.removeLast());
+        deque.offer((queue.remove()));
+        deque.offer((queue.remove()));
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,25 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> list = new ArrayList<>();
+        Queue<Integer> linkedList = new LinkedList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            linkedList.add(i);
+        }
+
+        while (!linkedList.isEmpty()) {
+            for (int i = 1; i < everyDishNumberToEat; i++) {
+                linkedList.add(linkedList.poll());
+            }
+            list.add(linkedList.poll());
+        }
+
+        return list;
     }
 }
